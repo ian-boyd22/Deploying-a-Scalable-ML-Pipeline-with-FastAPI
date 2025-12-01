3698a45b3f28c9d91aefa1f4aa6c98d14c6ffbdd
ianboyd36 <iboyd36@wgu.edu>
2025-11-30 21:10:40 -0600

    D501 Workflow

--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,22 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
-
+This current model uses random forest classifier from the sklearn package for the classification tasks. 
 ## Intended Use
-
+The intended use of this program is predict whether a person makes over 50k or not based on the census data provided by the class. 
 ## Training Data
+The training data is census data from 1996 that was collected by Ron Kohavi.
+The data within the rows respent individuals and their demograhpic along with their income information. 
+The training data was 80% of the total dataset. 
 
 ## Evaluation Data
-
+The evaluation data is the last 20% of the data that was split. processed the same way as the training data. 
 ## Metrics
 _Please include the metrics used and your model's performance on those metrics._
-
+Precision: 0.74
+Recall: 0.64
+Fbeta: 0.69
 ## Ethical Considerations
-
+This dataset could be bias since it has information on demographic information like race and sex. Also this data can't be used for any real decision making in the real world. 
 ## Caveats and Recommendations
+I would recommend that the data is updated as the data is very old and can't be used in any real world decision making.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,63 @@
 import pytest
+import pandas as pd
+import numpy as np
+from ml.data import process_data
+from ml.model import compute_model_metrics
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_test_split():
     """
-    # add description for the first test
+    # This is a test to check train-test split functionality
     """
-    # Your code here
-    pass
+    data = pd.DataFrame({
+        'workclass': ['Private', 'Self-emp-not-inc', 'Local-gov'],
+        'education': ['Bachelors', 'HS-grad', 'Masters'],
+        'age': [25, 38, 28]
+    })
 
+    train = data.sample(frac=0.8, random_state=42)
+    test = data.drop(train.index)
+
+    assert len(train) == 2
+    assert len(test) == 1
+    assert isinstance(train, pd.DataFrame)
+    assert isinstance(test, pd.DataFrame)  
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics():
     """
-    # add description for the second test
+    # This is a test to check compute_model_metrics functionality
     """
-    # Your code here
-    pass
+    y_true_value = np.array([1, 0, 1, 1, 0, 1])
+    y_pred_value = np.array([1, 0, 0, 1, 0, 1]) 
+
+    precision, recall, fbeta = compute_model_metrics(y_true_value, y_pred_value)
+
+    assert precision == 1.0
+    assert recall == 0.75
+    assert round(fbeta, 4) == 0.8571
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_process_data():
     """
-    # add description for the third test
+    # This is a test to check process_data functionality
     """
-    # Your code here
-    pass
+    census_data = pd.DataFrame({
+        'workclass': ['Private', 'Self-emp-not-inc', 'Local-gov'],
+        'age': [25, 38, 28],
+        'salary': ['<=50K', '>50K', '<=50K']
+    }) 
+
+    x, y, encoder, lb = process_data(
+        census_data,
+        categorical_features=['workclass'],
+        label='salary',
+        training=True)
+    
+    assert isinstance(x, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    assert x.shape[0] == 3
+    assert len(y) == 3
